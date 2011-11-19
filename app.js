a145d25dab69965cc269aa111ea35009ca16d27e
Cody <cody.cg@gmail.com>
2011-11-19 00:38:10 -0700

    moves images to /images

--- app.js
+++ app.js
@@ -45,14 +45,14 @@ app.post('/upload', function(req, res, next){
 	// parse a file upload
 	var form = new formidable.IncomingForm();
 	form.keepExtensions = true;
-	form.uploadDir = __dirname + '/public'
+	form.uploadDir = __dirname + '/public/images'
 	form.parse(req, function(err, fields, files) {
 		if (err) next(err);
 		// res.writeHead(200, {'content-type': 'text/plain'});
 		// res.write('received upload:\n\n');
 		// res.end(sys.inspect({fields: fields, files: files}));
 		var path = files.upload.path.split('/');
-		res.redirect('/' + path[path.length-1]);
+		res.redirect('/images/' + path[path.length-1]);
 	});
 });
 
