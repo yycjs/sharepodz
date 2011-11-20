7c4a42cfc10512e9a1d377242a7e8a0826523378
Dave Weston <dave.weston@binocracy.com>
2011-11-19 18:54:43 -0700

    Add popular tags info and split refine search results to a partial

--- routes/listing.js
+++ routes/listing.js
@@ -19,28 +19,40 @@ app.post('/listing/create', function(req, res, next){
 	});
 });
 
+function getPopularTags(req, res, next) {
+    Listing.getPopularTags(5, function(err, tags) {
+        var popTags = [];
+        for (var i = 0; i < tags.length; i++) {
+            popTags.push({tag:tags[i]});
+        }
+
+        req.popularTags = popTags;
+        next();
+    });
+}
+
 app.get('/listing/search*', function(req, res, next) {
     res.render('listing/search', { layout: 'listing/search_layout'});
 });
 
-app.get('/listing/results', function(req, res, next) {
+app.get('/listing/results', getPopularTags, function(req, res, next) {
     Listing.find({}, function(err, listings) {
+        console.log(listings);
         res.render('listing/results', {
-            locals: {results: listings}
+            locals: {results: listings, popularTags: req.popularTags}
         });
     });
 });
 
 app.get('/listing/:id', function(req, res, next){
-	Listing.findById(req.params.id, function(err, listing) {
+    Listing.findById(req.params.id, function(err, listing) {
 
-		sys.inspect(listing, true);
+	    sys.inspect(listing, true);
 
-		res.render('listing/show', {
-			locals: {listing: listing}
-		});
+	    res.render('listing/show', {
+   	        locals: {listing: listing, popularTags: req.popularTags}
+	    });
 
-	});    
-
+	});
 });
 
