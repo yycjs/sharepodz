cdf140aece040ac674399ba1c0567e4ca7dd044e
Eric <e.kryski@gmail.com>
2011-11-20 02:52:09 -0700

    changing to port 80

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ fs.readdir( './routes', function( err, files ) {
 
 	mongooseAuth.helpExpress(app);
 
-	app.listen(3000);
+	app.listen(80);
 	console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 });
