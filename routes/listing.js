f3c2699770ba74195125a4ee9edd8d20fd3ad722
Cory Smith <cory.m.smith@gmail.com>
2011-11-20 16:40:07 -0700

    got tags working

--- routes/listing.js
+++ routes/listing.js
@@ -2,54 +2,53 @@ var formidable = require('formidable');
 var sys = require('sys');
 var geocoder = require('geocoder');
 
-app.get('/listing/new', requireAuthorization, function(req, res, next){
+app.get('/listing/new', requireAuthorization, function(req, res, next) {
     res.render('listing/new');
 });
 
-app.post('/listing/create', requireAuthorization, function(req, res, next){
+app.post('/listing/create', requireAuthorization, function(req, res, next) {
     var errors = [];
     req.onValidationError(function(msg) {
         errors.push(msg);
     });
-    
-    
-    
+
+
     //Input Validation
     req.assert('name', 'You gotta name your space!').notNull();
-    req.assert('about', 'You should provide a little search description').notNull();
+    req.assert('tagline', 'You should provide a little search description').notNull();
     req.assert('description', 'You need to provide a listing description').notNull();
     req.assert('city', 'You need to provide a city').notNull();
     req.assert('province', 'You need to provide a province').notNull();
     req.assert('address1', 'You need to provide an address').notNull();
     req.assert('postalCode', 'You need to provide an postal code').is(/^\d{5}-\d{4}|\d{5}|[A-Z]\d[A-Z] \d[A-Z]\d$/);
     req.assert('phone', 'You need to provide a phone number').is(/^(?:\+?1[-. ]?)?\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/);
-    
+
     if (req.body.website != '')
         req.assert('website', 'If your going to provide a website you need to provide a valid one').isUrl();
-        
+
     if (req.body.twitter != '')
         req.assert('twitter', 'If your going to provide a twitter handle you need to provide a valid one').is(/^@/);
-        
+
     req.assert('email', 'You need to provide an email').isEmail();
     req.assert('startDate', 'You can\'t have a start date before today').isAfter();
     req.assert('endDate', 'You can\'t have an end date before today').isAfter();
-    
+
     if (errors.length) {
         res.send('There were problems: ' + errors.join(', '), 500);
         return;
     }
 
-    var tag_ids = req.body.tags;
-    req.body.tags = Tag.find({_id:{$in: tag_ids}}, function (err) {
-        if (err) {
-            console.log(err);
-            throw err;
-        }
-    });
+    // req.body.tag_ids are strings so we need to convert them to ObjectId's
+    var tagObjectIds = new Array();
+    for (index in req.body.tag_ids) {
+        var objectId = req.body.tag_ids[index];
+        tagObjectIds.push(mongoose.mongo.BSONPure.ObjectID.fromString(objectId));
+    }
+    req.body.tags = tagObjectIds;
 
     var listing = new Listing();
     var lat, lng, geocode_addr;
-    
+
     listing.owner = req.user.id;
     listing.name = req.body.name;
     listing.tagline = req.body.tagline;
@@ -68,21 +67,22 @@ app.post('/listing/create', requireAuthorization, function(req, res, next){
     listing.tags = req.body.tags;
     listing.images = [];
     listing.price = req.body.price;
-    
+
     geocode_addr = listing.address1 + ", " + listing.address2 + ", " + listing.city + ", " + listing.province;
-    geocoder.geocode(geocode_addr, function ( err, data ) {
+    geocoder.geocode(geocode_addr, function (err, data) {
         lat = data.results[0].geometry.location.lat;
         lng = data.results[0].geometry.location.lng;
-        
+
         listing.location = [lng, lat];
 
-        listing.save(function(err, doc){
+        listing.save(function(err, doc) {
             if (err) next(new Error('DB Error: Cannot Save Listing'));
             else {
                 var now = new Date();
                 console.log(now + ' - Created listing ' + doc.id);
                 res.redirect('/listing/' + doc.id);
-            };
+            }
+            ;
         });
     });
 });
@@ -142,16 +142,23 @@ app.get('/listing/browse', getPopularTags, function(req, res, next) {
     });
 });
 
-app.get('/listing/:id', function(req, res, next){
+app.get('/listing/:id', function(req, res, next) {
     Listing.findById(req.params.id, function(err, listing) {
-	    sys.inspect(listing, true);
-        
+        sys.inspect(listing, true);
+        var tags = new Array();
         listing.lat = listing.location[1];
         listing.lng = listing.location[0];
-        
-	    res.render('listing/show', {
-   	        locals: {listing: listing, popularTags: req.popularTags}
-	    });
-	});
+
+        Tag.find({_id : {$in : listing.tags}}, function(err, foundTags) {
+            if (!err) {
+                listing.tagobjects = foundTags;
+            }
+            res.render('listing/show', {
+                locals: {listing: listing, popularTags: req.popularTags}
+            });
+        });
+
+
+    });
 });
 
