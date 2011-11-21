5742360c22300747a0099d459629f9aed3298f4c
Joaquin Windmuller <joaquin@windmuller.ca>
2011-11-20 19:27:21 -0700

    Styling listing/results

--- public/js/search_results.js
+++ public/js/search_results.js
@@ -1,15 +1,31 @@
 (function($) {
-	$(function() {
-		var top_limit = $('#top').height();
-		var $related = $('.related');
+	var top_limit;
+	var $related;
+	function _moveMap(top) {
+		if (top <= top_limit) {
+			top = 0;
+		} else {
+			top = top - top_limit;
+		}
+	    $related.css('top', top + "px")
+	}
+	function enableScrollingMap() {
+		var relatedHeight = $related.offset().top + $related.height();
+		if ($(window).height() < relatedHeight) return;
 		$(window).scroll(function() {
-		    var top = $(this).scrollTop();
-			if (top <= 150) {
-				top = 0;
-			} else {
-				top = top - top_limit;
-			}
-			$related.css('top', top + "px");
+			_moveMap($(this).scrollTop());
 		});
+	}      
+	function scrollMapToCurrent() {
+		var $listings = $('.listing');
+		$listings.hover(function() {
+			_moveMap($(this).offset().top-30);
+		})
+	}
+	$(function() {
+		top_limit = $('#promo').offset().top + $('#promo').height() + 40;
+		$related = $('.related');
+		enableScrollingMap();
+		scrollMapToCurrent();
 	});
 })(jQuery);
