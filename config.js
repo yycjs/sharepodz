84ae118b9fa1450979646bc8137fc7a39bcb3a3b
David Luecke <daff@neyeon.de>
2011-12-17 16:24:29 -0700

    Mongodb connection fix

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ module.exports = {
     }
 };
 
-module.exports.connection = "mongodb://localhost/opendesk";
-//module.exports.connection = "mongodb://opendesk:beourguest@dbh85.mongolab.com:27857/opendesk?auto_reconnect";
+// module.exports.connection = "mongodb://localhost/opendesk";
+module.exports.connection = "mongodb://opendesk:beourguest@dbh85.mongolab.com:27857/opendesk?auto_reconnect";
 
 //module.exports.hostname = "http://sharepodz.com";
 module.exports.hostname = "http://localhost:3000";
